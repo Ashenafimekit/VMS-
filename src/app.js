c4049aa9a636deb3e9f1c35078c8135c76d744b9
Ashenafi Mekit <ashumekit502@gmail.com>
2024-12-15 00:38:43 +0300

    final commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://vms-kn4z.onrender.com"], // Allow both frontend URLs
+    origin: ["http://localhost:5173", 'https://vms-five.vercel.app',], // Allow both frontend URLs
   })
 );
 app.use(express.json());
