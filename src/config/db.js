ad609d108141a9df1d112ac6d685c5d830ad6ac9
Ashenafi Mekit <ashumekit502@gmail.com>
2024-12-15 00:14:03 +0300

    deployment

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(process.env.Db_URI);
     console.log("Database Connected!")
   } catch (error) {
     console.log("Database connection faild : ", error.message);
